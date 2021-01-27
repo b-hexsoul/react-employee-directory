b5a56b636146dd3b74ab6bc22b147df7c8054972
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-01-26 23:56:45 -0800

    'update'

---
+++ src/components/EmployeeContainer/employee-container.component.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class EmployeeContainer extends Component {
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/components/Heading/heading.component.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./heading.styles.css";
+
+export function Heading() {
+  return <h1>Employee Directory</h1>;
+}
