607bd39e638e5055d658306a5400cb340a88f40d
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-01-27 01:22:26 -0800

    'Search Bar Component'

--- src/components/SearchBar/search-bar.component.jsx
+++ src/components/SearchBar/search-bar.component.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
 
 export function SearchBar(props) {
   return (
-    <input
-      type="search"
-      placeholder="Name"
-      onChange={() => {
-        props.handleInput();
-      }}
-    />
+    <Form.Group className="mt-3">
+      <Form.Control
+        type="search"
+        placeholder="Name"
+        onChange={() => {
+          props.handleInput();
+        }}
+      />
+    </Form.Group>
   );
 }
