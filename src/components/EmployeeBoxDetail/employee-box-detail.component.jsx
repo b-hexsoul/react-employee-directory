02c96a6dc9f5e8a3230850a3fd178bca4a5aeedc
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-02-03 00:44:02 -0800

    'Modal'

--- src/components/EmployeeBoxDetail/employee-box-detail.component.jsx
+++ src/components/EmployeeBoxDetail/employee-box-detail.component.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
+import Modal from "react-bootstrap/Modal";
+import Col from "react-bootstrap/Col";
 
-export function EmployeeBoxDetail(props) {
-  if (Object.keys(props.employee).length === 0) {
-    return (
-      <Card>
-        <h1>Hello</h1>
-      </Card>
-    );
-  }
-
+export function EmployeeBoxDetail({ employee, showModal, handleClose }) {
   return (
-    <Card>
-      <Card.Body className="text-center">
-        <Image src={props.employee.picture.large} roundedCircle />
-        <h4>
-          {props.employee.name.first} {props.employee.name.last}
-        </h4>
-        <h4>{props.employee.email}</h4>
-        <h4>{props.employee.phone}</h4>
-      </Card.Body>
-    </Card>
+    <>
+      <Modal show={showModal} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <Image className="mx-5" src={employee.picture.large} roundedCircle />
+            {employee.name.first} {employee.name.last}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Col className="text-center">
+            <p>{employee.email}</p>
+            <p>{employee.phone}</p>
+            <p>
+              {employee.location.city}, {employee.location.state}
+            </p>
+          </Col>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
